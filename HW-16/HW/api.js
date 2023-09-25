e19106ae53993c54953c20e94e2019f7163ce764
Verbatum <verbatum@rambler.ru>
2023-09-25 22:25:47 +0300

    Ch

--- HW-16/HW/api.js
+++ HW-16/HW/api.js
@@ -1,15 +1,15 @@
 const host = 'https://wedev-api.sky.pro/api/v2/aleksey-kuzmenchuk/comments';
-const loginHost = "https://wedev-api.sky.pro/api/user/login";
+const regHost = 'https://webdev-hw-api.vercel.app/api/user';
+const loginHost = 'https://webdev-hw-api.vercel.app/api/user/login';
 
-/* export let token;
+let token = null;
 
-export const setToken = (newToken) => {
-  token = newToken;
-} */
-
-export const getComments = () => {
+export const getComments = ({token}) => {
     return fetch(host, {
         method: "GET",
+        headers: {
+          Authorization: token,
+        },
       })
         .then((response) => {
           if (response.status === 500) {
@@ -23,13 +23,13 @@ export const getComments = () => {
 export const postComments = ({textarea, nameInput}) => {
     return fetch(host, {
         method: "POST",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
         body: JSON.stringify({
           text: textarea.value,
           name: nameInput.value,
         }),
+        headers: {
+          Authorization: token,
+        },
       })
         .then((response) => {
           console.log(response);
@@ -43,14 +43,39 @@ export const postComments = ({textarea, nameInput}) => {
         })
 };
 
-export function fetchLogin(login, password) {
-  return fetch(loginHost, {
+export function registerUser({ login, password, name }) {
+
+  return fetch(regHost, {
     method: 'POST',
     body: JSON.stringify({
       login,
       password,
+      name
     }),
   }).then((response) => {
+    if (response.status === 400){
+      throw new Error ("Такой пользователь уже существует")
+    }    
     return response.json();
   })
 };
+
+export function loginUser({ login, password }) {
+    
+  return fetch(loginHost, {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+      
+    }),
+  }).then((response) => {
+      if (response.status === 400){
+       
+        throw new Error ("Неверный логин или пароль")
+      }
+      
+      return response.json()
+      
+  });
+}
