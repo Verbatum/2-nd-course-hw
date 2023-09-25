15fa2093f418099a7142ac900da72eddd3ebcb51
Verbatum <verbatum@rambler.ru>
2023-09-26 00:03:24 +0300

    Ch

--- HW-16/HW/renderComents.js
+++ HW-16/HW/renderComents.js
@@ -1,16 +1,15 @@
-
 export const renderComments = ({comments, list, addButton, textarea, initLikesListeners}) => {
     const commentsHTML = comments
       .map((comment, index) => {
         return `
           <li class="comment" data-item=${index}>
             <div class="comment-header">
-              <div>${sanitizeHtml(comment.author)}</div>
+              <div>${comment.author}</div>
               <div>${comment.date}</div>
             </div>
             <div class="comment-body">
               <div class="comment-text">
-                ${sanitizeHtml(comment.comment)}
+                ${comment.comment}
               </div>
             </div>
             <div class="comment-footer">
