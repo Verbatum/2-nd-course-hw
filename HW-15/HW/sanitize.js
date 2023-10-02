56815fbec71e3c534cd8900526fe0409aab84705
Verbatum <140429364+Verbatum@users.noreply.github.com>
2023-10-02 22:22:42 +0300

    Delete HW-15 directory

--- HW-15/HW/sanitize.js
+++
@@ -1,7 +0,0 @@
-export const sanitizeHtml = (htmlString) => {
-    return htmlString
-        .replaceAll("<", "&lt")
-        .replaceAll(">", "&gt")
-        .replaceAll("&", "&amp")
-        .replaceAll('"', "&quot")
-};
